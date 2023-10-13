41926726947dc3eafd76961b48aeca1e8518a830
bartek0 <methkill@gmail.com>
2023-10-13 10:29:29 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import hotBg from "./assets/hot.jpg";
+import coldBg from "./assets/cold.jpg";
+import Descriptions from "./components/Descriptions";
+import { useEffect, useState } from "react";
+import { getFormattedWeatherData } from "./weatherService";
+
+function App() {
+
+  const [city, setCity] = useState("Paris");
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
+  const [bg, setBg] = useState(hotBg);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(city, units);
+      setWeather(data);
+    };
+
+    fetchWeatherData();
+  }, []);
+
+  return (
+    <div className="app" style={{ backgroundImage: `url(${coldBg})`}}>
+      <div className="overlay">
+        {weather && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input
+                type="text"
+                name="city"
+                placeholder="Enter City..."
+              />
+              <button>°F</button>
+            </div>
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL}
+                  alt="weatherIcon"/>
+                <h3>{weather.description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} °${
+                  units === "metric" ? "C" : "F"
+                }`}</h1>
+              </div>
+            </div>
+            {/*bottom description*/}
+            <Descriptions weather={weather} units={units} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
