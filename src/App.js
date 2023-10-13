95a8e357f811886de28e91b67cac30b26e3baa4b
bartek0 <methkill@gmail.com>
2023-10-13 11:12:00 +0200

    - city search feature - dynamic background image

--- src/App.js
+++ src/App.js
@@ -15,10 +15,14 @@ function App() {
     const fetchWeatherData = async () => {
       const data = await getFormattedWeatherData(city, units);
       setWeather(data);
+
+      const threshold = units === "metric" ? 20 : 60;
+      if (data.temp <= threshold) setBg(coldBg);
+      else setBg(hotBg);
     };
 
     fetchWeatherData();
-  }, [units]);
+  }, [units, city]);
 
   const handleUnitsClick = (e) => {
     const button = e.currentTarget;
@@ -29,13 +33,20 @@ function App() {
     setUnits(isCelsius ? "metric" : "imperial");
   };
 
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  };
+
   return (
-    <div className="app" style={{ backgroundImage: `url(${coldBg})`}}>
+    <div className="app" style={{ backgroundImage: `url(${bg})`}}>
       <div className="overlay">
         {weather && (
           <div className="container">
             <div className="section section__inputs">
-              <input
+              <input onKeyDown={enterKeyPressed}
                 type="text"
                 name="city"
                 placeholder="Enter City..."
